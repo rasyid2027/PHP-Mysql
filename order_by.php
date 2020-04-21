b89d2bca751b513d179fd16bdd77d90446f03669
rasyid2027 <halimurrasyid56@gmail.com>
2020-04-21 08:18:44 +0700

    sixth commit

---
+++ order_by.php
@@ -0,0 +1,32 @@
+<!-- OOP -->
+
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "password";
+$dbname = "learn";
+
+//create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+//check connection
+if( $conn->connect_error ) {
+    die("Connection Failed:" . $conn->connect_error);
+}
+
+$sql = "SELECT id, firstname, lastname FROM MyGuest ORDER BY lastname DESC";
+$result = $conn->query($sql);
+
+if( $result->num_rows > 0 ) {
+    //output data of each rows
+    while( $row = $result->fetch_assoc() ) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+}  else {
+    echo "0 results.";
+}
+
+
+$conn->close();
+
+?>
